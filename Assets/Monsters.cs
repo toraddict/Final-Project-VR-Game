fdcf788bc0b08f958cdc297789d991032b060694
Mustafa Alhajjaj <dr.mustafa3@gmail.com>
2018-05-03 14:05:48 -0500

    First Commit

---
+++ Assets/Monsters.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class monsters : MonoBehaviour
+{
+    //declare the transform of our goal (where the navmesh agent will move towards) and our navmesh agent (in this case our zombie)
+    public Transform goal;
+    public NavMeshAgent agent;
+
+    // Use this for initialization
+    virtual public void Start()
+    {
+        //create references
+        goal = Camera.main.transform;
+        agent = GetComponent<NavMeshAgent>();
+        //set the navmesh agent's desination equal to the main camera's position (our first person character)
+        agent.destination = goal.position;
+        //start the walking animation
+        GetComponent<Animation>().Play("walk");
+
+    }
+
+}
---
+++ Assets/TriggerStart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+/*
+This message is sent to the trigger Collider and the Rigidbody(if any) that the trigger Collider belongs to, 
+and to the Rigidbody(or the Collider if there is no Rigidbody) that touches the trigger.Notes: Trigger events
+are only sent if one of the Colliders also has a Rigidbody attached.Trigger events will be sent to disabled
+MonoBehaviours, to allow enabling Behaviours in response to collisions.OnTriggerEnter occurs on the FixedUpdate
+after a collision. The Colliders involved are not guaranteed to be at the point of initial contact.*/
+
+public class Trigger
+{
+    public class trigger : MonoBehaviour
+    {
+        public virtual void OnTriggerEnter(Collider other)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
+
